e024213d41944ceb3347608c88d7bd9374e0725b
André <u202217772@upc.edu.pe>
2025-06-02 00:41:28 -0500

    feat(paymentCards): added new methods to kafka consumer

--- src/main/java/com/dynoware/cargosafe/paymentcardsservice/paymentCards/application/internal/queryservices/PaymentCardQueryServiceImpl.java
+++ src/main/java/com/dynoware/cargosafe/paymentcardsservice/paymentCards/application/internal/queryservices/PaymentCardQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dynoware.cargosafe.paymentcardsservice.paymentCards.application.internal.queryservices;
 
-
+import com.dynoware.cargosafe.paymentcardsservice.kafka.consumer.PaymentCardKafkaConsumer;
+import com.dynoware.cargosafe.paymentcardsservice.kafka.producer.PaymentCardKafkaProducer;
 import com.dynoware.cargosafe.paymentcardsservice.paymentCards.domain.model.aggregates.PaymentCard;
 import com.dynoware.cargosafe.paymentcardsservice.paymentCards.domain.model.queries.GetPaymentCardByIdQuery;
 import com.dynoware.cargosafe.paymentcardsservice.paymentCards.domain.services.PaymentCardQueryService;
@@ -12,13 +13,31 @@ import java.util.Optional;
 @Service
 public class PaymentCardQueryServiceImpl implements PaymentCardQueryService {
     private final PaymentCardRepository repository;
+    private final PaymentCardKafkaProducer kafkaProducer;
+    private final PaymentCardKafkaConsumer kafkaConsumer;
 
-    public PaymentCardQueryServiceImpl(PaymentCardRepository repository) {
+    public PaymentCardQueryServiceImpl(PaymentCardRepository repository,
+                                       PaymentCardKafkaProducer kafkaProducer,
+                                       PaymentCardKafkaConsumer kafkaConsumer) {
         this.repository = repository;
+        this.kafkaProducer = kafkaProducer;
+        this.kafkaConsumer = kafkaConsumer;
     }
 
     @Override
     public Optional<PaymentCard> handle(GetPaymentCardByIdQuery query) {
-        return repository.findById(query.id());
+
+        kafkaProducer.sendCardCreatedEvent(query.id());
+
+
+        Optional<Long> optionalCardId = kafkaConsumer.getLatestCardId();
+        if (optionalCardId.isEmpty()) {
+            throw new IllegalStateException("No se pudo obtener información de la tarjeta desde Kafka");
+        }
+
+        Long cardId = optionalCardId.get();
+
+
+        return repository.findById(cardId);
     }
 }
