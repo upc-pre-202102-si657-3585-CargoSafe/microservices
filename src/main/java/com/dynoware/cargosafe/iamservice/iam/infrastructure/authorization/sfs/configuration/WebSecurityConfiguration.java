b5419cceacba6d9e34160c7f18ae0b2ce08d1ac9
SergioPecan <u20201a938@upc.edu.pe>
2025-06-03 16:10:07 -0500

    feat(iam): completed feature iam

--- src/main/java/com/dynoware/cargosafe/iamservice/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/dynoware/cargosafe/iamservice/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -82,6 +82,7 @@ public class WebSecurityConfiguration {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(
+                                "/internal/token/**",
                                 "/api/v1/authentication/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
